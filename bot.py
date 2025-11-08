0c0ea340ea9018f24ca2b4ed3874fdac1ebe7db2
Abhradeep-2008 <74490559+Abhradeep-2008@users.noreply.github.com>
2025-11-08 19:51:31 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from discord.ext import commands
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Logged in as {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello! I am alive 😄")
+
+TOKEN = os.environ.get("TOKEN")
+bot.run(TOKEN)
